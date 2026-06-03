dda95621ed78dd1b2eb87f72da0f0f4938d2ba61
Akshaya-K-D <akshaya.dash27@gmail.com>
2026-06-03 12:26:54 +0530

    Update all_imx8_android_V15_dailybuild.sh

--- imx8/android/all_imx8_android_V15_dailybuild.sh
+++ imx8/android/all_imx8_android_V15_dailybuild.sh
@@ -1,1 +1,14 @@
-..
+#!/bin/bash
+
+MACHINE_LIST=""
+SOC_NAME=""
+
+if [ "$RSB3720A1" == true ]; then
+    MACHINE_LIST="rsb3720_a1"
+    SOC_NAME="imx8mp"
+fi
+
+export MACHINE_LIST
+export SOC_NAME
+
+./imx8_android_V15_dailybuild.sh
