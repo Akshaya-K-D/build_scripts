f9bd332c0f043532165825df58e144e5ebcd9552
Akshaya-K-D <akshaya.dash27@gmail.com>
2026-09-18 14:05:00 +0530

    Update all_risc_qcs_android15_dailybuild.sh

--- qcs/all_risc_qcs_android15_dailybuild.sh
+++ qcs/all_risc_qcs_android15_dailybuild.sh
@@ -1,41 +1,31 @@
 #!/bin/bash
 set -e
 
-###############################################################################
-# Qualcomm QCS6490 Android 15 - All Projects Daily Build
-###############################################################################
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+echo "===================================================="
+echo "[ADV] QCS Android 15 Daily Build"
+echo "===================================================="
 
 if [ "${aom2721a1:-false}" = "true" ]; then
-
-    echo "[ADV] AOM2721A1 Android 15 Daily Build"
-
+    echo "[ADV] Project: AOM2721A1"
     export PROJECT="aom2721a1"
     export PRODUCT="AOM2721A1"
-
-    ./risc_qcs_android15_dailybuild.sh
+    "${SCRIPT_DIR}/risc_qcs_android15_dailybuild.sh"
 fi
-
 
 if [ "${aom5721a1:-false}" = "true" ]; then
-
-    echo "[ADV] AOM5721A1 Android 15 Daily Build"
-
+    echo "[ADV] Project: AOM5721A1"
     export PROJECT="aom5721a1"
     export PRODUCT="AOM5721A1"
-
-    ./risc_qcs_android15_dailybuild.sh
+    "${SCRIPT_DIR}/risc_qcs_android15_dailybuild.sh"
 fi
-
 
 if [ "${ds011a1:-false}" = "true" ]; then
-
-    echo "[ADV] DS011A1 Android 15 Daily Build"
-
+    echo "[ADV] Project: DS011A1"
     export PROJECT="ds011a1"
     export PRODUCT="DS011A1"
-
-    ./risc_qcs_android15_dailybuild.sh
+    "${SCRIPT_DIR}/risc_qcs_android15_dailybuild.sh"
 fi
 
-
-echo "[ADV] All done!"
+echo "[ADV] All QCS Android 15 builds done!"
